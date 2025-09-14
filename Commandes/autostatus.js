03b3bfba76aff69df58cebc0f6eb7d1573bff34e
NIGHT SHADOW <thompsontcholvens@gmail.com>
2025-09-14 14:39:04 -0400

    Update autostatus.js

--- Commandes/autostatus.js
+++ Commandes/autostatus.js
@@ -7,7 +7,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'SHADOW BOT',
             serverMessageId: -1
         }
     }
