17d62a13a9a1f1f3ec2ecdc162e7a4d3efee7312
NIGHT SHADOW <thompsontcholvens@gmail.com>
2025-09-18 14:05:21 -0400

    Create Setting.js

---
+++ Setting.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ownerNumber: ['50932593561,50933932981'],  // Numéro du propriétaire
+  botName: 'SHADOW BOT',
+  channelLink: 'https://whatsapp.com/channel/0029VbBdHP4KWEKiD3YlC70b', // Lien de notre chaîne WhatsApp
+  commandPrefix: '🌹',
+  autoJoinRequired: true, // Active la vérification d’abonnement
+  }
